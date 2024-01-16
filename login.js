fde5e09e121fb95d1b95a93b34df7f08f95f9ce1
c.ferrara <chiara.ferrara@stevejobs.academy>
2024-01-16 12:02:58 +0100

    feat(login.js): counter working

--- login.js
+++ login.js
@@ -2,37 +2,79 @@ const email = "";
 const emails = JSON.parse(localStorage.getItem("emails")) || [];
 const today = Date.now();
 
-
-const welcomeMessage = () =>{
+const welcomeMessage = () => {
   const user = getLoggedEmail();
   document.getElementById("containerbox").innerHTML = `
   <h1>Benvenut*</h1><br><p>${user}</p><br>
-  <button id="logoutBtn">Logout</button>`
-  document.getElementById('logoutBtn').addEventListener("click", function(){
+  <button id="logoutBtn" class="btn btn-dark">Logout</button>`;
+  document.getElementById("logoutBtn").addEventListener("click", function () {
     clearCurrentUserEmail();
     loginMessage();
-  })
-  
-}
+  });
+};
 
+const welcomeBackMessage = () => {
+  const user = getLoggedEmail();
+  const currentUser = getUserLogged();
+  document.getElementById("containerbox").innerHTML = `
+  <p>${currentUser.counter}</p>
+  <h1>Bentornat*</h1><br><p>${user}</p><br>
+  <button id="logoutBtn" class="btn btn-dark">Logout</button>`;
+  document.getElementById("logoutBtn").addEventListener("click", function () {
+    clearCurrentUserEmail();
+    loginMessage();
+  });
+};
 
 const loginMessage = () => {
-  document.getElementById('containerbox').innerHTML = `
+  document.getElementById("containerbox").innerHTML = `
   <form id="emailForm" name="emailform" action="#">
-  <input type="email" id="inputEmail" name="email"  size="30" required />
-  <button type="submit" id="submitBtn"  onclick="checkEmail(document.emailform.email)">Login</button>
-</form>`
-document.getElementById("submitBtn").addEventListener("click", function () {
-  onClickBtnLogin();
-});
-}
-
+  <input type="email" class="form-control"  id="inputEmail" name="email"  size="30" required />
+  <button class="btn btn-dark" type="submit" id="submitBtn"  onclick="checkEmail(document.emailform.email)">Login</button>
+</form>`;
+  document.getElementById("submitBtn").addEventListener("click", function () {
+    onClickBtnLogin();
+  });
+};
 
 const onClickBtnLogin = () => {
-  saveCurrentUserEmail()
-  addEmail();
+  const user = getUserLogged();
+  console.log(user)
+  const prevUsers = JSON.parse(localStorage.getItem("emails"));
+  console.log(prevUsers);
+  const email = document.getElementById("inputEmail").value;
+  const existingUser = emails.find((u) => u.email === email);
+ saveCurrentUserEmail()
+
+
+  if(existingUser){   
+    updateUser();
+    welcomeBackMessage();
+  } else{
+    saveCurrentUserEmail()
+    addEmail();
+    welcomeMessage();
+  }
+  
   getLoggedEmail();
-  welcomeMessage();
+};
+
+const updateUser = () => {
+  const prevUsers = JSON.parse(localStorage.getItem("emails"));
+  const emailLogged = getLoggedEmail();
+  const newUsers = prevUsers.map((user) => {
+    if (user.email === emailLogged) {
+      return {
+        ...user,
+        lastlogged: new Date(),
+        counter: user.counter + 1,
+      };
+    } else {
+      return { ...user };
+    }
+  });
+  emails.splice(0, emails.length, ...newUsers);
+  localStorage.setItem("emails", JSON.stringify(newUsers));
 };
 
 const addEmail = () => {
@@ -52,32 +94,33 @@ const addEmail = () => {
   }
 };
 
-const updateEmail = () => {
-  const prevUsers = JSON.parse(localStorage.getItem("emails")) || [];
-
-}
-
-const saveCurrentUserEmail = () =>{
+const saveCurrentUserEmail = () => {
   const input = document.getElementById("inputEmail");
   const emailValidated = checkEmail(input);
   if (emailValidated === true) {
-  const email = document.getElementById("inputEmail").value;
-  localStorage.setItem("email", email);}
-    else{
-      alert(`Invalid E-mail`)
-    }
-}
+    const email = document.getElementById("inputEmail").value;
+    localStorage.setItem("email", email);
+  } else {
+    alert(`Invalid E-mail`);
+  }
+};
 
-const clearCurrentUserEmail = () =>{
+const clearCurrentUserEmail = () => {
   localStorage.removeItem("email");
-}
+};
 
 const getLoggedEmail = () => {
   // metodo per ottenere la mail dell'utente connesso
   const currentLoggedEmail = localStorage.getItem("email");
   console.log(`E-mail dell'utente connesso:` + currentLoggedEmail);
   return currentLoggedEmail;
-  
+};
+
+const getUserLogged = () =>{
+  const emailLogged = getLoggedEmail();
+  const prevUsers = JSON.parse(localStorage.getItem("emails")) || [];
+  const user = prevUsers.find((user) => user.email === emailLogged);
+  return user;
 }
 
 const checkEmail = (input) => {
@@ -92,21 +135,16 @@ const checkEmail = (input) => {
   }
 };
 
-
-
-
-
-
 document.getElementById("emailForm").addEventListener("submit", function () {
   event.preventDefault(); //deprecato? funziona
 });
 
-window.onload=() =>{
+window.onload = () => {
   const email = getLoggedEmail();
   const isLogged = !!email;
-  if(isLogged) {
+  if (isLogged) {
     welcomeMessage();
   } else {
     loginMessage();
   }
-}
+};
